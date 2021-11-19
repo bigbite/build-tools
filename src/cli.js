0082a414c67b5814b71985712db9f5b38ff95dc2
ampersarnie <ampersarnie@gmail.com>
2021-11-19 15:41:52 +0000

    fix: remove old build references

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
-const webpack = require('webpack');
-
-const build = require('./build');
 
 yargs.scriptName('bbbt');
 yargs.usage('Usage: bbbt <command>');
@@ -13,30 +10,3 @@ yargs.alias('v', 'version');
 yargs.commandDir('./commands');
 yargs.demandCommand(1, 'A command is required.');
 yargs.parse();
-return;
-console.log(argv);
-
-const config = {
-  project: argv._[0] ? argv._[0] : '',
-  allProjects: argv.site && !argv._[0] ? false : true,
-  mode: argv.prod || argv.p ? 'production' : 'development',
-};
-
-console.log(config);
-
-const webpackConfig = build(config);
-
-const compiler = webpack(webpackConfig);
-
-if (!argv.S || !argv.single) {
-  compiler.watch(
-    {
-      aggregateTimeout: 300,
-    },
-    (err, stats) => {
-      console.log(err, stats);
-
-      process.stdout.write(stats.toString() + '\n');
-    },
-  );
-}
