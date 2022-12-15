23225ff799d15605618ff489e7ef77a128565ba0
Paul Taylor <ampersarnie@gmail.com>
2022-12-15 17:36:53 +0000

    test: add example style to test structure

--- example-site/plugins/test-plugin/src/entrypoints/editor.js
+++ example-site/plugins/test-plugin/src/entrypoints/editor.js
@@ -2,6 +2,8 @@ import DisplayComponent from '@Components/DisplayComponent';
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 
+import '../styles/index.scss';
+
 registerBlockType('bigbite/build-tools-block', {
   title: __('Build Tools Block', 'bigbite-build-tools'),
   category: 'common',
