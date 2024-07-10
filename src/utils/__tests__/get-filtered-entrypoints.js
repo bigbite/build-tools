070f02876e902f6aa67153a165adc9e2a82c260f
Connor Brough <connor.brough@yahoo.com>
2024-07-10 11:34:05 +0100

    build: fix tests

--- src/utils/__tests__/get-filtered-entrypoints.js
+++ src/utils/__tests__/get-filtered-entrypoints.js
@@ -8,13 +8,13 @@ describe('Get filtered entrypoints', () => {
   });
 
   it('Returns object with array of entrypoints', () => {
-    const projects = ['test-project@frontend+editor'];
+    const projects = 'test-project@frontend+editor';
     const result = getFilteredEntryPoints(projects);
     expect(result).toEqual({ 'test-project': ['frontend', 'editor'] });
   });
 
   it('Return object with entrypoints where @ is present and empty array if non defined', () => {
-    const projects = ['test-project@editor', 'test-project-2'];
+    const projects = 'test-project@editor,test-project-2';
     const result = getFilteredEntryPoints(projects);
     expect(result).toEqual({ 'test-project': ['editor'], 'test-project-2': []});
   });
