9c41b1fadea84de165c002d7bda7c0eb4eb7de3f
Paul Taylor <paul@bigbite.net>
2024-10-29 18:24:00 +0000

    test: add number and sprintf usage examples

--- example-site/plugins/test-plugin/src/components/DisplayComponent.tsx
+++ example-site/plugins/test-plugin/src/components/DisplayComponent.tsx
@@ -1,28 +1,32 @@
 import SVGAsComponent from '@Static/logo.svg';
 import SVGAsURL from '@Static/svg-file.svg?url';
 import MailIconPNG from '@Static/mail-icon.png';
-import { __ } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
 
 import TestComponent from './TestComponent';
 
 /**
  * Display Component for an example block.
  */
-export default (): JSX.Element => {
+export default ({ attributes: { itemCount = 2 } }): JSX.Element => {
+  // General comment.
   // translators: This is some basic alt-text.
   const svgAlt = __("Reference and SVG as the url", 'test-translation');
 
+  // translators: %d is the number of items chosen.
+  const translatedValue = sprintf(_n("%d item", "%d items", itemCount, 'test-translation'), itemCount);
+
   return (
-  <>
-    <div className="logo">
-      <SVGAsComponent />
-      <img src={SVGAsURL} alt={svgAlt} />
-    </div>
-    <div>Example Block</div>
-    <TestComponent additionalValue="This is not hidden" />
-    <div>
-      <img src={MailIconPNG} alt="PNG Mail Icon for testing usage." />
-    </div>
-  </>
-);
+    <>
+      <div className="logo">
+        <SVGAsComponent />
+        <img src={SVGAsURL} alt={svgAlt} />
+      </div>
+      <div>Example Block</div>
+      <TestComponent additionalValue={translatedValue} />
+      <div>
+        <img src={MailIconPNG} alt="PNG Mail Icon for testing usage." />
+      </div>
+    </>
+  );
 }
