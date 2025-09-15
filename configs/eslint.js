19cb01506aaa4586210669211046fefb074564a1
Gareth Elwell <41474928+g-elwell@users.noreply.github.com>
2025-09-15 21:35:55 +0100

    fix: remove static config interfering with dynamic aliases

--- configs/eslint.js
+++ configs/eslint.js
@@ -1,5 +1,3 @@
-const { eslintResolver } = require('./../src/utils/get-alias');
-
 module.exports = {
   extends: [
     'plugin:@wordpress/eslint-plugin/recommended-with-formatting', // no prettier, using the version with prettier in the plugin will cause a conflict
@@ -10,9 +8,6 @@ module.exports = {
     es2021: true,
     node: true,
     jest: true,
-  },
-  settings: {
-    'import/resolver': eslintResolver(),
   },
   rules: {
     'jsdoc/require-returns-description': 0, // we don't always need a description
